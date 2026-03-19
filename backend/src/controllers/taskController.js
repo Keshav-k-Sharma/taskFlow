8946646316cc46d6e7d0870a6b8d9ec1255d42f9
keshav krishan sharma <reach.keshavks@gmail.com>
2026-03-19 09:40:21 +0530

    fix :fix member deployment issue

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -1,6 +1,5 @@
 const task = require("../models/task");
 
-
 const getAlltasks = async (req, res) => {
     try {
         const tasks = await task.find()
@@ -12,45 +11,35 @@ const getAlltasks = async (req, res) => {
     }
 };
 
-const CreateTask= async (req,res) => {
-    try
-    {
-        const {title ,description ,assignedTo ,deadline ,status } =req.body ;
-
-        const newtask= await task.create({title ,description ,assignedTo ,deadline ,status , createdBy : req.user._id});
-
+const CreateTask = async (req, res) => {
+    try {
+        const { title, description, assignedTo, deadline, status } = req.body;
+        const newtask = await task.create({
+            title, description, assignedTo, deadline, status,
+            createdBy: req.user._id
+        });
         res.status(201).json(newtask);
-
-
-    }catch(error){
-        return res.status(500).json({message:error.message});
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
     }
+};
 
-}
-
-const UpdateStatus= async (req,res) => {
-    try
-    {
+const UpdateStatus = async (req, res) => {
+    try {
         const { id } = req.params;
-        const{ status }= req.body;
-
-        const updatedtask = await  task.findByIdAndUpdate(
+        const { status } = req.body;
+        const updatedtask = await task.findByIdAndUpdate(
             id,
-            {status},
-            {new :true}
+            { status },
+            { new: true }
         );
-
-        if(!updatedtask){
-            return res.status(404).json({message: "tasks not found "}); 
+        if (!updatedtask) {
+            return res.status(404).json({ message: "Task not found" });
         }
-
         res.status(200).json(updatedtask);
-
-
-    }catch(error){
-        return res.status(500).json({message:error.message});
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
     }
+};
 
-}
-
-module.exports = {CreateTask, UpdateStatus, getAlltasks};
+module.exports = { CreateTask, UpdateStatus, getAlltasks };
